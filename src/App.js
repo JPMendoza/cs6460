2363d5006ac2969892db29f2dcf36d5099e4e01c
Jorge <jmendoza35@gatech.edu>
2018-10-26 21:01:51 -0700

    adding more stuff and getting routing working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {BrowserRouter, Route, Switch } from "react-router-dom"
 import './App.css';
+import Layout from "./components/Layout"
+import Home from "./components/Home"
+import BasicCourse from "./components/BasicCourse"
+import FunFacts from "./components/FunFacts"
+import Error from "./components/Error"
+import Nav from "./components/Nav"
+
+
 
 class App extends Component {
   render() {
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React 
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <div class="container">
+          <Nav/>
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="/basiccourse" component={BasicCourse}/>
+            <Route path="/funfacts" component={FunFacts}/>
+            <Route component={Error}/>
+          </Switch>
+        </div>
+      </BrowserRouter>
+
     );
   }
 }
