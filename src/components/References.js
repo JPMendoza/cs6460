ceb368e3936f5c93b4524d57ce87a40f0c38c5c2
Jorge <jmendoza35@gatech.edu>
2018-11-03 20:17:17 -0700

    milestone 1 updates

--- src/components/References.js
+++ src/components/References.js
@@ -6,7 +6,7 @@ export default class References extends React.Component {
        <div>
           References <br/>
           In app comming soon, please see the following link
-          <a href="https://docs.google.com/document/d/1_5J_Y7wjRWudWWbepfbtmP0XK1ejZAZ2hLviEO2gLqs"> References </a>
+          <a target="blank" href="https://docs.google.com/document/d/1_5J_Y7wjRWudWWbepfbtmP0XK1ejZAZ2hLviEO2gLqs"> References </a>
        </div>
 
     );
